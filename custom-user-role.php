16dbd26b86fcfdf83917badc7151d43282c14132
Saddam Hossain Azad <saddam987020@gmail.com>
2019-01-28 18:27:19 +0600

    Added plugin files.

---
+++ custom-user-role.php
@@ -0,0 +1,233 @@
+<?php
+/**
+ * Plugin Name: Custom User Role
+ * Plugin URI: http://oskyblue.com
+ * Version: 1.0
+ * Description: Woocommerce percentage based discounts on products by custom user role.
+ * Author: Osky Blue
+ * Author URI: http://www.oskyblue.com
+**/
+
+if ( ! defined( 'ABSPATH' ) ) { 
+    exit; // Exit if accessed directly
+}
+
+if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+  return;
+}
+
+// Define contants
+define('CUR_ROOT', dirname(__FILE__));
+define('CUR_URL', plugins_url('/', __FILE__));
+define('CUR_HOME', home_url('/'));
+
+//require_once( ABSPATH . '/wp-content/plugins/woocommerce/woocommerce.php' );
+require_once( CUR_ROOT . '/includes/custom-sales-and-po.php' );
+
+// create custom plugin settings menu
+/*add_action('admin_menu', 'custom_user_role_menu');*/
+
+function custom_user_role_menu() {
+
+	//create new top-level menu
+	add_submenu_page('options-general.php', 'Custom User Role Settings', 'Custom User Role Settings', 'administrator', __FILE__, 'custom_user_role_settings_page');
+	add_submenu_page('options-general.php', 'Discounts', 'Discounts', 'administrator', 'custom-discounts-page', 'custom_discounts_page');
+
+	//call register settings function
+	/*add_action( 'admin_init', 'register_custom_user_role_settings' );*/
+}
+
+
+function register_custom_user_role_settings() {
+	//register our settings
+	register_setting( 'custom-user-role-settings-group', 'cur_user_roles' );
+	register_setting( 'custom-user-role-settings-group', 'cur_prod_categories' );
+	//register_setting( 'custom-user-role-settings-group', 'option_etc' );
+}
+
+function custom_user_role_settings_page() {
+	if( isset($_POST['submit']) ) {
+		$cur_user_role = $_POST['cur_user_roles'];
+		update_option('cur_user_roles', $cur_user_role);
+		$prod_args = array( 'hide_empty' => false );
+		$prod_categories = get_terms('product_cat', $prod_args);
+		foreach( $prod_categories as $cat ) {
+			$cur_prod_cat_discount = $_POST['cur_prod_cat_'.$cat->term_id];
+			update_option($cur_user_role.'.cur_prod_cat_'.$cat->term_id, $cur_prod_cat_discount);
+		}
+		echo '<div class="updated"><p>Discounts added Successfully</p></div>';
+	}
+?>
+<div class="wrap">
+<h2>Custom User Role</h2>
+
+<form method="post" action="">
+    <?php //settings_fields( 'custom-user-role-settings-group' ); ?>
+    <?php //do_settings_sections( 'custom-user-role-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">User Roles</th>
+        <td>
+        	<select name="cur_user_roles">
+        		<?php wp_dropdown_roles(); ?>
+            </select>
+        </td>
+        </tr>
+         
+        <tr valign="top">
+        <th scope="row">Product Categories</th>
+        <td>
+			<?php
+                $args = array( 'hide_empty' => false );
+                $product_categories = get_terms('product_cat', $args);
+                foreach( $product_categories as $cat ) {
+                    //echo '<option value="'.$cat->term_id.'"' . selected( esc_attr(get_option('cur_prod_categories')), $cat->term_id, false ) . '>'.$cat->name.'</option>';
+                    echo '<p><label style="width: 120px; display: inline-block;">'.$cat->name.'</label><input type="text" name="cur_prod_cat_'.$cat->term_id.'" />'.' <small style="color: #666666;">Percentage up to 3 decimal places</small></p>';
+                }
+            ?>
+        </td>
+        </tr>
+        
+    </table>
+    
+    <?php submit_button(); ?>
+
+</form>
+</div>
+<?php
+}
+
+function custom_discounts_page() {
+?>
+<div class="wrap">
+<h2>Discounts</h2>
+<table class="form-table">
+<tr style="border-bottom: 1px solid #e2e2e2;">
+	<th style="text-align:center; padding-right:0;">User Role</th>
+	<th style="text-align:center; padding-right:0;">Product Category</th>
+	<th style="text-align:center; padding-right:0;">Discount(%)</th>
+</tr>
+<?php
+	global $wp_roles;
+	foreach ( $wp_roles->roles as $key=>$value ):
+		//echo $key.'=';
+		//echo $value['name'].'<br />';
+		$args = array( 'hide_empty' => false );
+		$product_categories = get_terms('product_cat', $args);
+		foreach( $product_categories as $cat ) {
+			echo '<tr style="border-bottom: 1px solid #e2e2e2;">';
+				echo '<td style="text-align:center;">'.$value['name'].'</td>';
+				echo '<td style="text-align:center;">'.$cat->name.'</td>';
+				echo '<td style="text-align:center;">'.esc_attr(get_option($key.'.cur_prod_cat_'.$cat->term_id)).'</td>';
+			echo '</tr>';
+		}
+    endforeach;
+?>
+</table>
+</div>
+<?php } ?>
+<?php
+//add_filter('woocommerce_get_regular_price','cur_return_custom_price', 10, 2);
+//add_filter('woocommerce_get_sale_price','cur_return_custom_price', 10, 2);
+add_filter('woocommerce_order_amount_item_subtotal', 'cur_return_custom_price', 10, 2);
+add_filter('woocommerce_get_price', 'cur_return_custom_price', 10, 2);
+function cur_return_custom_price($price, $product) {
+	global $post, $woocommerce;
+	$product_id = $product->id; 
+
+	// Get user's ip location and correspond it to the custom field key
+	/*$user_country = $_SESSION['user_location'];
+	$get_user_currency = strtolower($user_country.'_price');*/
+	// If the IP detection is enabled look for the correct price
+	/*if($get_user_currency!=''){
+		$new_price = get_post_meta($post_id, $get_user_currency, true);
+		if($new_price==''){
+			$new_price = $price;
+		}
+	}*/
+
+	if ( is_user_logged_in() ) {
+		//$user_role = get_user_role();
+		$user_ID = get_current_user_id();
+		$product_category = get_product_category_by_id( $product_id );
+		$discount = get_the_author_meta($user_ID.'_cur_prod_cat_'.$product_category, $user_ID);
+		if( isset($discount) ) {
+			$discount_price = $price*($discount/100);
+			$price = $price - $discount_price;
+		}
+	}
+
+	return $price;
+}
+
+function get_user_role() {
+	global $current_user;
+
+	$user_roles = $current_user->roles;
+	$user_role = array_shift($user_roles);
+
+	return $user_role;
+}
+
+function get_product_category_by_id( $product_id ) {
+	$term_list = wp_get_post_terms($product_id,'product_cat',array('fields'=>'ids'));
+	$cat_id = (int)$term_list[0];
+	return $cat_id;
+	//echo get_term_link ($cat_id, 'product_cat');
+}
+
+
+add_action( 'show_user_profile', 'cur_show_extra_profile_fields' );
+add_action( 'edit_user_profile', 'cur_show_extra_profile_fields' );
+function cur_show_extra_profile_fields( $user ) { ?>
+	<h3>Custom Discount Info</h3>
+	<table class="form-table">
+		<!--<tr>
+			<th><label for="active">Active</label></th>
+			<td>
+				<select class="text-select" name="active" id="active">
+					<option <?php //if(get_the_author_meta( 'active', $user->ID ) == 'Yes') echo 'selected="selected"'; ?> value="Yes">Yes</option>
+					<option <?php //if(get_the_author_meta( 'active', $user->ID ) == 'No') echo 'selected="selected"'; ?> value="No">No</option>
+				</select>
+			</td>
+		</tr>-->
+		
+		<tr>
+			<th>Product Categories</th>
+			<td>
+				<?php
+					$args = array( 'hide_empty' => false );
+					$product_categories = get_terms('product_cat', $args);
+					$read_only = '';
+					if( ! current_user_can( 'manage_options' ) ) {
+						$read_only = 'readonly="readonly"';
+					}
+
+					foreach( $product_categories as $cat ) {
+						echo '<p><label style="width: 120px; display: inline-block;">'.$cat->name.'</label><input type="text" name="cur_prod_cat_'.$cat->term_id.'" value="'.get_the_author_meta( $user->ID.'_cur_prod_cat_'.$cat->term_id, $user->ID ).'" '.$read_only.' />'.' <small style="color: #666666;">Percentage up to 3 decimal places</small></p>';
+					}
+				?>
+			</td>
+		</tr>
+	</table>
+<?php }
+
+
+add_action( 'personal_options_update', 'cur_save_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'cur_save_extra_profile_fields' );
+function cur_save_extra_profile_fields( $user_id ) {
+
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+
+	// Copy and paste this line for additional fields.
+	/*update_usermeta( $user_id, 'active', $_POST['active'] );*/
+
+	$prod_args = array( 'hide_empty' => false );
+	$prod_categories = get_terms('product_cat', $prod_args);
+	foreach( $prod_categories as $cat ) {
+		$cur_prod_cat_discount = $_POST['cur_prod_cat_'.$cat->term_id];
+		update_user_meta($user_id, $user_id.'_cur_prod_cat_'.$cat->term_id, $cur_prod_cat_discount);
+	}
+}
+?>
---
+++ includes/custom-sales-and-po.php
@@ -0,0 +1,222 @@
+<?php
+/**
+* Change Proceed To Checkout Text in WooCommerce
+* Place this in your Functions.php file
+**/
+
+function woocommerce_button_proceed_to_checkout() {
+	$checkout_url = WC()->cart->get_checkout_url();
+	?>
+	<a href="<?php echo $checkout_url; ?>" class="checkout-button button alt wc-forward"><?php _e( 'Order', 'woocommerce' ); ?></a>
+<?php
+}
+
+
+add_filter( 'woocommerce_payment_gateways', 'cur_remove_unused_payment_gateways', 20, 1 );
+/**
+ *  This function will remove all of the WooCommerce standard gateways from the 
+ *  WooCommerce > Settings > Checkout dashboard.
+ */
+function cur_remove_unused_payment_gateways( $load_gateways ) {
+	$remove_gateways = array( 
+		'WC_Gateway_BACS',
+		'WC_Gateway_Cheque',
+		'WC_Gateway_COD',
+		'WC_Gateway_Paypal',
+		'WC_Gateway_Simplify_Commerce',
+		'WC_Addons_Gateway_Simplify_Commerce'
+	);
+
+	foreach ( $load_gateways as $key => $value ) {
+		if ( in_array( $value, $remove_gateways ) ) {
+			unset( $load_gateways[ $key ] );
+		}
+	}
+	return $load_gateways;
+}
+
+
+add_action( 'plugins_loaded', 'init_default_no_gateway_class' );
+function init_default_no_gateway_class() {
+	class WC_Default_No_Gateway extends WC_Payment_Gateway {
+
+		public function __construct() {
+			$this->id                 = 'owp';
+			$this->method_title       = __( 'Order without Payment', 'woocommerce' );
+			$this->method_description = __( 'Have your customers order without any online payment.', 'woocommerce' );
+			$this->has_fields         = false;
+			// Load the settings
+			$this->init_form_fields();
+			$this->init_settings();
+			// Get settings
+			$this->title              = $this->get_option( 'title' );
+			$this->description        = $this->get_option( 'description' );
+			$this->instructions       = $this->get_option( 'instructions', $this->description );
+			$this->enable_for_methods = $this->get_option( 'enable_for_methods', array() );
+			$this->enable_for_virtual = $this->get_option( 'enable_for_virtual', 'yes' ) === 'yes' ? true : false;
+			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		}
+		
+		/**
+		 * Initialise Gateway Settings Form Fields
+		 */
+		public function init_form_fields() {
+			$shipping_methods = array();
+			if ( is_admin() )
+				foreach ( WC()->shipping()->load_shipping_methods() as $method ) {
+					$shipping_methods[ $method->id ] = $method->get_title();
+				}
+			$this->form_fields = array(
+				'enabled' => array(
+					'title'       => __( 'Enable OWP', 'woocommerce' ),
+					'label'       => __( 'Enable Order without Payment', 'woocommerce' ),
+					'type'        => 'checkbox',
+					'description' => '',
+					'default'     => 'yes'
+				),
+				'title' => array(
+					'title'       => __( 'Title', 'woocommerce' ),
+					'type'        => 'text',
+					'description' => __( 'Payment method description that the customer will see on your checkout.', 'woocommerce' ),
+					'default'     => __( 'Order without Payment', 'woocommerce' ),
+					'desc_tip'    => true,
+				),
+				'description' => array(
+					'title'       => __( 'Description', 'woocommerce' ),
+					'type'        => 'textarea',
+					'description' => __( 'Payment method description that the customer will see on your website.', 'woocommerce' ),
+					'default'     => __( 'Order without any online payment.', 'woocommerce' ),
+					'desc_tip'    => true,
+				),
+				/*'instructions' => array(
+					'title'       => __( 'Instructions', 'woocommerce' ),
+					'type'        => 'textarea',
+					'description' => __( 'Instructions that will be added to the thank you page.', 'woocommerce' ),
+					'default'     => __( 'Order without any online payment.', 'woocommerce' ),
+					'desc_tip'    => true,
+				),*/
+				'enable_for_methods' => array(
+					'title'             => __( 'Enable for shipping methods', 'woocommerce' ),
+					'type'              => 'multiselect',
+					'class'             => 'wc-enhanced-select',
+					'css'               => 'width: 450px;',
+					'default'           => '',
+					'description'       => __( 'If OWP is only available for certain methods, set it up here. Leave blank to enable for all methods.', 'woocommerce' ),
+					'options'           => $shipping_methods,
+					'desc_tip'          => true,
+					'custom_attributes' => array(
+						'data-placeholder' => __( 'Select shipping methods', 'woocommerce' )
+					)
+				),
+				'enable_for_virtual' => array(
+					'title'             => __( 'Accept for virtual orders', 'woocommerce' ),
+					'label'             => __( 'Accept OWP if the order is virtual', 'woocommerce' ),
+					'type'              => 'checkbox',
+					'default'           => 'yes'
+				)
+		   );
+		}
+
+		function process_payment( $order_id ) {
+			global $woocommerce;
+			$order = new WC_Order( $order_id );
+		
+			// Mark as on-hold (we're awaiting the cheque)
+			$order->update_status('on-hold', __( 'Payment to be made upon confirmation', 'woocommerce' ));
+		
+			// Reduce stock levels
+			$order->reduce_order_stock();
+		
+			// Remove cart
+			$woocommerce->cart->empty_cart();
+		
+			// Return thankyou redirect
+			return array(
+				'result' => 'success',
+				'redirect' => $this->get_return_url( $order )
+			);
+		}
+
+	}
+}
+
+
+function add_default_no_gateway_class( $methods ) {
+	$methods[] = 'WC_Default_No_Gateway'; 
+	return $methods;
+}
+add_filter( 'woocommerce_payment_gateways', 'add_default_no_gateway_class' );
+
+
+function hide_payment_methods() {
+	if( is_checkout() ) {
+?>
+	<style type="text/css">
+		#payment .payment_methods { display: none; }
+		ul.order_details li.method { display: none; }
+		table.order_details tfoot > tr:nth-last-child(2) { display: none; }
+	</style>
+<?php
+	}
+}
+add_action( 'wp_head', 'hide_payment_methods' );
+
+
+function change_checkout_title( $title ) {
+	if( is_checkout() && in_the_loop() ) {
+		$title = 'Order';
+	}
+	return $title;
+}
+add_filter( 'the_title', 'change_checkout_title' );
+
+
+add_filter( 'manage_edit-shop_order_columns', 'cur_set_custom_column_order_columns');
+function cur_set_custom_column_order_columns($columns) {
+	// global $woocommerce;
+	$new_col_array = array();
+	foreach($columns as $key => $title) {
+		if ($key=='shipping_address') {
+			$new_col_array['items']  = __( 'Items', 'woocommerce' );
+		}
+		$new_col_array[$key] = $title;
+	}
+    return $new_col_array;
+}
+
+add_action( 'manage_shop_order_posts_custom_column' , 'cur_custom_shop_order_column', 10, 2 );
+function cur_custom_shop_order_column( $column ) {
+	global $post, $woocommerce, $the_order;
+
+    switch ( $column ) {
+
+        case 'items' :
+            $terms = $the_order->get_items();
+            //$terms = $the_order;
+
+	    if ( is_array( $terms ) ) {
+			foreach($terms as $term) {
+				echo $term['item_meta']['_qty'][0] .' x ' . $term['name'] .'<br />';
+			}
+        } else {
+            _e( '____', 'woocommerce' );
+			//print_r($terms);
+		}
+
+		break;
+
+    }
+}
+
+
+function hide_order_payment_method() {
+	if( is_account_page() ) {
+?>
+	<style type="text/css">
+		table.order_details tfoot > tr:nth-last-child(2) { display: none; }
+	</style>
+<?php
+	}
+}
+add_action( 'wp_head', 'hide_order_payment_method' );
+?>
